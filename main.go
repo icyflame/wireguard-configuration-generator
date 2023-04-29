0fac331b75eb65a1fef1e1901bd56bbfa9b49f7b
Siddharth Kannan <mail@siddharthkannan.in>
2023-04-29 16:30:05 +0900

    Automatically generate the keys for peers in a given configuration

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/icyflame/wireguard-configuration-generator/internal/configuration"
+	"github.com/icyflame/wireguard-configuration-generator/internal/keygen"
 )
 
 const (
@@ -17,17 +18,19 @@ const (
 func main() {
 	err, returnCode := _main()
 	if err != nil {
-		log.Fatalf("exit with error", err)
+		log.Fatal("exit with error: ", err)
 	}
 	os.Exit(returnCode)
 }
 
 func _main() (error, int) {
 	var configurationFile string
+	var keysBaseDirectory string
 	flag.StringVar(&configurationFile, "configuration-file", "", "configuration file for generating wireguard configurations")
+	flag.StringVar(&keysBaseDirectory, "keys-base-dir", "", "base directory for storing the private and public keys required for wireguard")
 	flag.Parse()
 
-	if configurationFile == "" {
+	if configurationFile == "" || keysBaseDirectory == "" {
 		flag.PrintDefaults()
 		return fmt.Errorf("insufficient arguments"), ExitErr
 	}
@@ -38,7 +41,21 @@ func _main() (error, int) {
 		return fmt.Errorf("could not read config file: %w", err), ExitErr
 	}
 
-	fmt.Printf("%#v", networkConfig)
+	fmt.Printf("%#v\n", networkConfig)
+
+	for networkName, config := range networkConfig {
+		err := configuration.Validate(config)
+		if err != nil {
+			return fmt.Errorf("configuration for network %s is invalid: %w", networkName, err), ExitErr
+		}
+
+		err = keygen.GenerateKeys(networkName, config, keysBaseDirectory)
+		if err != nil {
+			return fmt.Errorf("could not generate all keys for %s: %w", networkName, err), ExitErr
+		}
+	}
+
+	fmt.Printf("configuration is valid")
 
 	return nil, ExitOK
 }
