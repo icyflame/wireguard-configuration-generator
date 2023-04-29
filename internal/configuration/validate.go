0dd0ef2d853af71a4a6af61a490687ed961f00bb
Siddharth Kannan <mail@siddharthkannan.in>
2023-04-29 16:29:33 +0900

    Add code for validating the incoming configuration

---
+++ internal/configuration/validate.go
@@ -0,0 +1,30 @@
+package configuration
+
+import (
+	"fmt"
+
+	"github.com/icyflame/wireguard-configuration-generator/internal/utils"
+)
+
+// Validate ...
+func Validate(config NetworkConfig) error {
+	var addresses []string
+	for _, client := range append(config.Clients, config.Server) {
+		addresses = append(addresses, client.Address)
+	}
+
+	if !utils.IsUnique(addresses) {
+		return fmt.Errorf("addresses must be unique")
+	}
+
+	var identifiers []string
+	for _, client := range append(config.Clients, config.Server) {
+		identifiers = append(identifiers, client.Identifier)
+	}
+
+	if !utils.IsUnique(identifiers) {
+		return fmt.Errorf("identifiers must be unique")
+	}
+
+	return nil
+}
